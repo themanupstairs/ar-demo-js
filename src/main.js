096da8f126f87c850ea80af57535ddd9ce646ac1
Jose Silva <jose.abilio.silva@gmail.com>
2025-08-25 14:02:37 +0100

    01: First commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const list = ["hat", "earring"];
+  const visibles = [false, true];
+  const setVisible = (button, entities, visible) => {
+    if (visible) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+    entities.forEach((entity) => {
+      entity.setAttribute("visible", visible);
+    });
+  };
+  list.forEach((item, index) => {
+    const button = document.querySelector("#" + item);
+    const entities = document.querySelectorAll("." + item + "-entity");
+    setVisible(button, entities, visibles[index]);
+    button.addEventListener("click", () => {
+      visibles[index] = !visibles[index];
+      setVisible(button, entities, visibles[index]);
+    });
+  });
+});
